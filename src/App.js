96a725e36c839945709aeebcbf5e8f808930ae63
Brendan Smith <brendan5320@gmail.com>
2021-05-25 17:37:56 -0500

    start of tuesday lab

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles/style.scss';
 
-// import Header from './header.js';
+import Header from './header.js';
 // import Form from './form.js';
-// import Footer from './footer.js';
+import Footer from './footer.js';
 import Form2 from './form2';
 import People from './people';
 
@@ -29,8 +29,10 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <Header />
         <Form2 prompt="click for star war" toggleLoading={this.toggleLoading} handler={this.handleForm} />
         <People people={this.state.results} />
+        <Footer />
       </React.Fragment>
     )
   }
